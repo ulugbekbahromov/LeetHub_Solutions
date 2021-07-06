0553210f94775f23eace6d53337d8f7bcd048eee
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2021-07-06 11:16:43 +0500

    Time: 1 ms (99.86%), Space: 40.1 MB (68.36%) - LeetHub

--- fizz-buzz/fizz-buzz.java
+++ fizz-buzz/fizz-buzz.java
@@ -10,17 +10,26 @@ class Solution {
             buzz++;
             
             if (fizz == 3 && buzz == 5) {
-                fizz = 0;
-                buzz = 0;
-                list.add("FizzBuzz");
+        
+                fizz = 0; // back to 0
+                buzz = 0; // back to 0
+                list.add("FizzBuzz"); // add "FizzBuzz"
+                
             } else if (fizz == 3) {
-                fizz = 0;
-                list.add("Fizz");
+                
+                fizz = 0; // back to zero
+                list.add("Fizz"); // add "Fizz"
+                
             } else if (buzz == 5) {
-                buzz = 0;
-                list.add("Buzz");
+                
+                buzz = 0; // back to zero
+                list.add("Buzz"); // add "Buzz"
+                
             } else {
+                
+                // add just number itself
                 list.add(Integer.toString(i));
+                
             }
         }
         
