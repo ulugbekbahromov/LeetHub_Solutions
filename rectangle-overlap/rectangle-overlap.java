203d2ef4f5da06b12a7439da7d7fdf9b4beea119
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2021-07-14 17:37:56 +0500

    Time: 0 ms (100.00%), Space: 36.3 MB (44.58%) - LeetHub

--- rectangle-overlap/rectangle-overlap.java
+++ rectangle-overlap/rectangle-overlap.java
@@ -1,28 +1,27 @@
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
         
-        if (rec1[0] < rec2[2]
-            && rec1[1] < rec2[3]
-            && rec2[0] < rec1[2]
-            && rec2[1] < rec1[3]) {
+        if (rec1[0] < rec2[2] && rec1[1] < rec2[3]
+           && rec2[0] < rec1[2] && rec2[1] < rec1[3]) {
             return true;
         }
-        
         return false;
     }
 }
 
 
 /*
+Logic : far points of rec2 should be bigger than of rec1 (a < d)
+        close points of rec1 should be gigger than of rec2 (c < b)
                  rec2
                  /
-            ---------
+            --------- -> d
             |       |
-        ----|----   |
+        ----|---->b |
         |   |   |   |
-        |   ----|----
+        |c <----|----
         |       |
-        ---------
+   a <- ---------
          
     rec1 /
 
