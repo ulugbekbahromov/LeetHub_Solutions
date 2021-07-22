60105d4de410100bd18b17f3609ce9a9c19e32a4
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2021-07-22 16:59:16 +0500

    Time: 0 ms (100.00%), Space: 37.9 MB (23.22%) - LeetHub

--- plus-one/plus-one.java
+++ plus-one/plus-one.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         
-        for (int i = digits.length - 1; i >= 0; i--) {
+        int n = digits.length;
+        for (int i = n-1; i >= 0; i--) {
             
             if (digits[i] < 9) {
                 digits[i]++;
@@ -11,7 +12,7 @@ class Solution {
             digits[i] = 0;
         }
         
-        int[] newArr = new int[digits.length + 1];
+        int[] newArr = new int[n+1];
         newArr[0] = 1;
         return newArr;
     }
