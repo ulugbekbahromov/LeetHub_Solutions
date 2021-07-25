58a7fad6324f663573600e06f739bcce1936bf71
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2021-07-25 11:43:59 +0500

    Time: 0 ms (100.00%), Space: 39.3 MB (26.30%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int maxSum = nums[0];
+        int curSum = maxSum;
+        
+        if (nums.length == 1) return nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            curSum = Math.max(nums[i] + curSum, nums[i]);
+            maxSum = Math.max(maxSum, curSum);
+        }
+        
+        return maxSum;
+    }
+}
+
+// Kadane's Algo, O(n)
