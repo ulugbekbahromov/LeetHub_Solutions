4fa25f916cb10f8c683debdbf63eb1d558cffb26
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2021-10-04 10:44:57 +0500

    Time: 1 ms (12.86%), Space: 39.8 MB (72.02%) - LeetHub

--- binary-search/binary-search.java
+++ binary-search/binary-search.java
@@ -1,21 +1,12 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        int low = 0;
-        int high = nums.length - 1;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[mid] < target)
-                low = mid+1;
-            else 
-                high = mid-1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
         }
         
-        return -1;
+        return  -1;
     }
 }
-
-// Classical Bnary Search - O(logn) time complexity
