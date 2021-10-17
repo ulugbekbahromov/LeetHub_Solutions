62712674f4b6b0cdbab8569a4ca1a79dc45643c1
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2021-10-17 06:17:05 +0500

    Time: 0 ms (100.00%), Space: 38.8 MB (94.11%) - LeetHub

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,27 +1,28 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
-        int[] newNums = new int[2];
+        int[] indexArr = new int[2];
         
         int left = 0;
         int right = nums.length - 1;
         
         while (left < right) {
-            
             int sum = nums[left] + nums[right];
             
             if (sum == target) {
-                newNums[0] = left + 1;
-                newNums[1] = right + 1;
+                indexArr[0] = ++left;
+                indexArr[1] = ++right;
                 break;
             } else if (sum > target) {
                 right--;
             } else {
                 left++;
-            }
+            }   
         }
         
-        return newNums;
+        return indexArr;
     }
 }
 // Two-pointer approach
+// Time Complexity - O(n)
+// Space Complexity - O(1)
