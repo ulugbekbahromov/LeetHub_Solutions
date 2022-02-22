c9cb84a4fa942c0db30f8c7cf830ddba963660dd
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-02-22 14:52:51 +0500

    Time: 303 ms (96.00%), Space: 0B (100.00%) - LeetHub

--- 175-combine-two-tables/175-combine-two-tables.sql
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -2,11 +2,6 @@
 select firstName, lastName, city, state
 from Person as p
 left join Address as a
-on p.personId = a.personId
-union
-select firstName, lastName, city, state
-from Person as p
-left join Address as a
 on p.personId = a.personId;
 
-# using union
+# using left join
