cd9adc0959cee319134d986266084ab245eab047
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-02-22 14:41:32 +0500

    Time: 325 ms (83.78%), Space: 0B (100.00%) - LeetHub

---
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select firstName, lastName, city, state
+from Person as p
+left join Address as a
+on p.personId = a.personId
+union
+select firstName, lastName, city, state
+from Person as p
+left join Address as a
+on p.personId = a.personId;
