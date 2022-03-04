6393a963dbc0c951a282afa1ac884075f460c239
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-03-04 10:59:25 +0500

    Time: 7 ms (47.88%), Space: 4.1 MB (40.91%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.go
@@ -0,0 +1,18 @@
+func fizzBuzz(n int) []string {
+    str := []string{}
+    
+    for i := 1; i <= n; i++ {
+    
+        if i % 5 == 0 && i % 3 == 0 {
+            str = append(str, "FizzBuzz")
+        } else if i % 5 == 0 {
+            str = append(str, "Buzz")
+        } else if i % 3 == 0 {
+            str = append(str, "Fizz")
+        } else {
+            str = append(str, strconv.Itoa(i))
+        }
+    }
+    
+    return str
+}
