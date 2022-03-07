ef18eb0a2557350c2a041ef0be0d934f388a9266
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-03-07 11:25:31 +0500

    Time: 856 ms (5.05%), Space: 8.5 MB (90.37%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.go
+++ 217-contains-duplicate/217-contains-duplicate.go
@@ -1,11 +1,12 @@
 func containsDuplicate(nums []int) bool {
-    m := make(map[int]int)
     
-    for _, v := range nums {
-        if m[v] == 1 {
-            return true
+    for i := 0; i < len(nums) - 1; i++ {
+        for j := i + 1; j < len(nums); j++ {
+            if nums[i] == nums[j] {
+                return true
+            }
         }
-        m[v]++
-    } 
+    }
+    
     return false
 }
