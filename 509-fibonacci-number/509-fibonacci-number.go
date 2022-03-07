bd926ef31882cbaca49d2778df401047aeb39594
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-03-07 10:53:57 +0500

    Time: 2 ms (56.49%), Space: 1.9 MB (43.18%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.go
+++ 509-fibonacci-number/509-fibonacci-number.go
@@ -1,16 +1,15 @@
+var m = make(map[int]int)
+
 func fib(n int) int {
-    if n <= 1 {
-        return n
+    
+    if n == 0 {
+        return 0
+    } else if n <= 2 {
+        return 1
+    } else if _, ok := m[n]; ok {
+        return m[n]
     }
     
-    a, b := 0, 1
-    sum := 0
-    
-    for ;n > 1 ;n-- {
-        sum = a + b
-        a = b
-        b = sum
-    }
-    
-    return b
+    m[n] = fib(n-1) + fib(n-2)
+    return m[n]
 }
