1643d68c964197b37ad0d77d2e168c85f8763215
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-05-03 12:22:55 +0500

    Time: 679 ms (12.23%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -2,5 +2,10 @@
 select customer_number
 from Orders
 group by customer_number
-order by count(*) desc
-limit 1;
+having count(order_number) = (
+            select count(*)
+            from Orders
+            group by customer_number
+            order by count(*) desc
+            limit 1
+);
