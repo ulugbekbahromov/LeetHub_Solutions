d37025fca1d74579523df26fe93081e12dc82c8a
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-05-03 12:27:42 +0500

    Time: 543 ms (29.07%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -1,11 +1,10 @@
 # Write your MySQL query statement below
 select customer_number
-from Orders
-group by customer_number
-having count(order_number) = (
-            select count(*)
-            from Orders
-            group by customer_number
-            order by count(*) desc
-            limit 1
-);
+from (
+    select 
+            customer_number,
+            dense_rank() over(order by count(order_number) desc) as 'ranking'
+    from orders
+    group by customer_number 
+) as sub
+where sub.ranking = 1;
