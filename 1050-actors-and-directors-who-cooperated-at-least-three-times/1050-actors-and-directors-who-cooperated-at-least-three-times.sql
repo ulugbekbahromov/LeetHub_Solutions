0792fb732c4c69c7e71dbfe2d749a2281485af3c
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-06-03 09:56:11 +0500

    Time: 746 ms (6.10%), Space: 0B (100.00%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
-select actor_id, director_id
-from ActorDirector
-group by actor_id, director_id
-having count(timestamp) >= 3;
+select distinct(actor_id), director_id
+from (
+    select 
+        actor_id, 
+        director_id,
+        count(timestamp) over(partition by actor_id, director_id) as number_of_times
+    from ActorDirector
+) as temp_table
+where number_of_times >= 3;
+
+# hard solutions for me now
