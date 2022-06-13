a051742096169999474756fbd7787013dfe29e76
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-06-13 21:15:42 +0500

    Time: 605 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,8 +1,6 @@
-select
-    ifnull (
-        (select distinct salary
-        from Employee
-        order by salary desc
-        limit 1 offset 1),
-        null
-) as SecondHighestSalary;
+select max(salary) as SecondHighestSalary
+from Employee
+where salary not in (
+    select max(salary)
+    from Employee
+);
