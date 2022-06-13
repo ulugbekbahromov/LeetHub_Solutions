307126f0bcbc1313a80c2b39f240246504d006b5
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-06-13 21:44:10 +0500

    Time: 456 ms (21.01%), Space: 0B (100.00%) - LeetHub

--- 178-rank-scores/178-rank-scores.sql
+++ 178-rank-scores/178-rank-scores.sql
@@ -1,12 +1,5 @@
-with S2 as (
-    select distinct score
-    from Scores
-)
-
-select 
-    S1.score, 
-    count(S2.score) as 'Rank' 
-from Scores as S1, S2
-where S1.score <= S2.score
-group by S1.id 
-order by S1.score desc;
+# Write your MySQL query statement below
+select
+    score,
+    dense_rank() over(order by score desc) as 'rank'
+from Scores;
