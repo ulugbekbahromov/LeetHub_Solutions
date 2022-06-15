48dc765029085f3411bf770c982b38128b05791e
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-06-15 15:49:45 +0500

    Time: 46 ms (93.20%), Space: 14.4 MB (65.74%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,5 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
-    
-# solution with sorted
+        dict1, dict2 = {}, {}
+        
+        for i in s:
+            dict1[i] = dict1.get(i, 0) + 1
+        
+        for i in t:
+            dict2[i] = dict2.get(i, 0) + 1
+
+        return dict1 == dict2
+
+# solution with dictionary
