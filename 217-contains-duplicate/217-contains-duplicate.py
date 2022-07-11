79dd7bc9c3fec7ecbe6ebda24f811e24f17957ce
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-07-11 23:02:45 +0500

    Time: 477 ms (91.00%), Space: 25.9 MB (72.31%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,13 +1,4 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashNum = {}
         
-        for i in nums:
-            if i not in hashNum:
-                hashNum[i] = 1    
-            else:
-                return True
-            
-        return False
-    
-# solution with hashtable
+        return len(set(nums)) != len(nums)
