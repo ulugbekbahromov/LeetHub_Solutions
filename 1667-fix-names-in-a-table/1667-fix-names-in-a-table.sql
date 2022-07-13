5ffd95d8029c65dc63ada3b50d81d2ba09b043e6
Ulugbek A. Bahrom <61583887+ulugbekbahromov@users.noreply.github.com>
2022-07-13 19:20:28 +0500

    Time: 820 ms (35.21%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,5 +1,5 @@
 select
     user_id,
-    concat(upper(substring(name, 1, 1)), lower(substring(name, 2))) as name
+    concat(ucase(left(name, 1)), lcase(right(name, length(name)-1))) as name
 from Users
 order by user_id;
